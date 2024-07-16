2c50e28a370e02ca4ae79bed93dac273b240b515
phi <phi.friday@gmail.com>
2024-07-17 00:40:29 +0900

    test: async ping

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -2,10 +2,11 @@
 # pyright: reportUnknownParameterType=false
 from __future__ import annotations
 
+import pytest
 import sqlalchemy as sa
 
 
-def test_ping(sync_session):
+def test_ping1(sync_session):
     stmt = sa.text("select 1")
     fetch = sync_session.execute(stmt)
     value = fetch.scalar_one()
@@ -16,6 +17,24 @@ def test_ping(sync_session):
 def test_ping2(sync_session):
     stmt = sa.text("select 1")
     fetch = sync_session.execute(stmt)
+    value = fetch.scalar_one()
+    assert isinstance(value, int)
+    assert value == 1
+
+
+@pytest.mark.anyio()
+async def test_ping3(async_session):
+    stmt = sa.text("select 1")
+    fetch = await async_session.execute(stmt)
+    value = fetch.scalar_one()
+    assert isinstance(value, int)
+    assert value == 1
+
+
+@pytest.mark.anyio()
+async def test_ping4(async_session):
+    stmt = sa.text("select 1")
+    fetch = await async_session.execute(stmt)
     value = fetch.scalar_one()
     assert isinstance(value, int)
     assert value == 1
