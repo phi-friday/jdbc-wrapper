ab0e85ae89dc8edbb7447d8466798308afebdf1b
phi <phi.friday@gmail.com>
2024-07-16 23:56:41 +0900

    feat: find_loader

--- src/jdbc_wrapper/_loader/__init__.py
+++ src/jdbc_wrapper/_loader/__init__.py
@@ -19,24 +19,27 @@ if TYPE_CHECKING:
 
     from jdbc_wrapper._loader.base import BaseLoader
 
-__all__ = ["SQliteLoader", "PostgresqlLoader", "MssqlLoader", "load"]
+__all__ = ["SQliteLoader", "PostgresqlLoader", "MssqlLoader", "load", "find_loader"]
 
 
 def load(
     backend: str, base_dir: str | PathLike[str] | None = None, /
 ) -> tuple[str, Sequence[Path]]:
     backend = backend.lower()
-    loader_class: type[BaseLoader]
-    if backend in {"sqlite", SQliteLoader.default_driver.lower()}:
-        loader_class = SQliteLoader
-    elif backend in {"postgresql", PostgresqlLoader.default_driver.lower()}:
-        loader_class = PostgresqlLoader
-    elif backend in {"mssql", MssqlLoader.default_driver.lower()}:
-        loader_class = MssqlLoader
-    else:
-        error_msg = f"Not found driver: {backend}"
-        raise ValueError(error_msg)
-
+    loader_class = find_loader(backend)
     loader = loader_class(base_dir=base_dir)
     paths = loader.load_latest()
     return loader.default_driver, paths
+
+
+def find_loader(backend: str, /) -> type[BaseLoader]:
+    backend = backend.lower()
+    if backend in {"sqlite", SQliteLoader.default_driver.lower()}:
+        return SQliteLoader
+    if backend in {"postgresql", PostgresqlLoader.default_driver.lower()}:
+        return PostgresqlLoader
+    if backend in {"mssql", MssqlLoader.default_driver.lower()}:
+        return MssqlLoader
+
+    error_msg = f"Not found driver: {backend}"
+    raise ValueError(error_msg)
